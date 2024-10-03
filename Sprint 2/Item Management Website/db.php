c122713f9aea96b6d02b9e1d7267d3c3184e1cfd
gabbie558 <enriquezgabriela558@gmail.com>
2024-10-03 15:40:20 +0800

    Create database connection

---
+++ Sprint 2/Item Management Website/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "enriquezzZ@558";
+$dbname = "item_management"; // Make sure this matches the name of the created database
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
